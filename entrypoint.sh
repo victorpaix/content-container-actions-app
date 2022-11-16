e6a8c0ec23ac0f415683e09571623460a14db194
Victorpaix <35776458+victorpaix@users.noreply.github.com>
2022-11-16 14:57:13 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello $1"   # uses the who-to-greet input variable to print "Hello [who-to-greet]" in the log file
+echo "Hello $INPUT_MYINPUT"   # uses the who-to-greet input variable to print "Hello [who-to-greet]" in the log file
+memory=$(cat /proc/meminfo)
 time=$(date)      # gets the current time and sets it as an output variable that actions running later in a job can use.
-echo "::set-output name=time::$time" # sets the action's output parameter using the workflow syntax
+echo "::set-output name=time::$memory" # sets the action's output parameter using the workflow syntax
